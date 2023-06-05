6fdbb50a8a7f67636c9f2b142aefc0bc3c773811
PunchedYou <nibot04@gmail.com>
2023-06-05 23:04:56 +0200

    updated redirects

--- next.config.js
+++ next.config.js
@@ -1,4 +1,40 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/r/statsfm",
+        destination: "https://stats.fm/nilsbtr",
+        permanent: false,
+      },
+      {
+        source: "/r/sunflowers",
+        destination: "https://discord.gg/7UjGrd9Qhb",
+        permanent: false,
+      },
+      {
+        source: "/r/raidreport",
+        destination: "https://raid.report/pc/4611686018485820917",
+        permanent: false,
+      },
+      {
+        source: "/r/dungeonreport",
+        destination: "https://dungeon.report/pc/4611686018485820917",
+        permanent: false,
+      },
+      {
+        source: "/r/nightfall",
+        destination: "https://nightfall.report/guardian/3/4611686018485820917",
+        permanent: false,
+      },
+      {
+        source: "/r/guardianreport",
+        destination:
+          "https://guardian.report/?view=RAIDS&guardians=4611686018485820917",
+        permanent: false,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
